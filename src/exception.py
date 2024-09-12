d1c65692fe86a6109d9e69e06515f79d8ff606f4
Akash <akashyallamati34@gmail.com>
2024-09-11 22:09:09 -0400

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+#Search exceptions python for documentation
+
+import sys # --> module used to control exception automatically
+import logging
+
+def error_message_detail(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename #custom exception handling documentation
+    error_message = "Error occured in py script [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno, str(error))
+    
+    return error_message #returns error message
+
+
+class customException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
